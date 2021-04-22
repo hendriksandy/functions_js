045179e828978523e7d3e54334ba1c4b0deb6312
hendriksandy <79459594+hendriksandy@users.noreply.github.com>
2021-04-22 23:19:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const makingPizza = function () {
+
+    console.log("maak je deeg en rol het uit");
+    console.log("Doe de tomatensaus op het deeg");
+    console.log("Doe je garnituur erop en als laatste de kaas");
+    console.log("10 minuut in een oven van 270 graden en smullen maar");
+};
+
+makingPizza();
+makingPizza();
+makingPizza();
+
+const howToMakeSushi = function () {
+
+    console.log("Doe de gekookte rijst op je nori");
+    console.log("Leg je garnituur op de rijst");
+    console.log("rol alles op en snij het");
+}
+
+howToMakeSushi();
+howToMakeSushi();
